11a6beb67ae334b03f305de3d459e40927194d5b
renato-albuquerque <renatomalb83@gmail.com>
2022-12-08 11:34:38 -0300

    project updated

---
+++ js/script.js
@@ -0,0 +1,59 @@
+import {Modal} from "./modal.js"
+
+// Variables
+const form = document.querySelector("form")
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
+/* const modalWrapper = document.querySelector(".modal-wrapper")
+const modalMessage = document.querySelector(".modal .title span")
+const modalBtnClose = document.querySelector(".modal button.close") */
+
+/* const Modal = {
+    wrapper: document.querySelector(".modal-wrapper"), // POPUP screen.
+    message: document.querySelector(".modal .title span"),
+    buttonClose: document.querySelector(".modal button.close"),
+
+    open() {
+        Modal.wrapper.classList.add("open")
+    },
+    close() {
+        Modal.wrapper.classList.remove("open")
+    }
+} */
+
+/* 03 ways to create and assign function to an event:
+form.onsubmit = function() {}
+
+form.onsubmit = () => {} //Arrow function
+
+form.onsubmit = handleSubmit
+function handleSubmit() {} */
+
+form.onsubmit = function(event) {
+    event.preventDefault() // Don't run the default. The default is: Load the webpage.
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const result = BMI(weight, height)    
+    const message = `Your BMI is ${result}`
+    
+    Modal.message.innerText = message
+    /* modalWrapper.classList.add("open") */
+    Modal.open()
+} 
+
+// Modal.buttonClose.onclick = () => { //Arrow function.
+    /* modalWrapper.classList.remove("open") */
+//    Modal.close()
+// }
+
+/* Another possibility:
+modalBtnClose.onclick = function() {
+    modalWrapper.classList.remove("open")
+} */
+
+function BMI(weight, height) {
+    return (weight / ((height/100) ** 2)).toFixed(2)
+}
